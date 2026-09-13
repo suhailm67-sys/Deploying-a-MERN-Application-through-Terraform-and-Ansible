520499b57c05f00746efaecdbd2037e35c59a77b
S.Mohamed Suhail <suhailm67@gmail.com>
2026-09-13 18:12:45 +0530

    Update AMI from Ubuntu 24.04 to 22.04

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,12 +1,11 @@
-# Get the latest Ubuntu 24.04 LTS AMI
+# Get the latest Ubuntu 22.04 LTS AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-
-  owners = ["099720109477"]
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
